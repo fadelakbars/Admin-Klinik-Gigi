6e75b65b4b7af3342f5cc50778f431caa1c3f287
fadlullah07 <ferfectkill@gmail.com>
2024-01-09 02:22:07 +0800

    form login //hanya tampilan belum berfungsi

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,19 +2,19 @@
 
 @section('form')
     <section class="vh-100" style="background-color: #61859a;">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col col-xl-10">
-                    <div class="card" style="border-radius: 1rem;">
-                        <div class="col-lg-14 d-flex align-items-center justify-content-center">
+        <div class="container d-flex justify-content-center align-items-center py-5 h-100">
+            <div class="row  h-100">
+                <div class="col col-xl-10 ">
+                    <div class="card" style="border-radius: 1rem; width:30rem;">
+                        <div class="col-lg-100 ">
                             <div class="card-body p-4 p-lg-5 text-black">
                                 <form action="">
 
-                                    <div class="w-25 h-25 d-flex align-items-center mb-3 pb-1">
-                                        <img class="img-fluid" src="{{ asset('style/assets/img/klinik-fifin.jpg') }}"
+                                    <div class="w-100 h-25 d-flex align-items-center mb-3 pb-1">
+                                        <img class="float-start img-fluid" style="max-width:4rem" src="{{ asset('style/assets/img/klinik-fifin.jpg') }}"
                                             alt="logo">
+                                        <h4 class="text-bold" style="margin-left: 1rem">FIFIN DENTAL KLINIK</h4>
                                     </div>
-
                                     <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Login ke akun anda</h5>
 
                                     <div class="form-outline mb-4">
